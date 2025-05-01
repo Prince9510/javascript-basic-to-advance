cdc5504c605a5b01289121b38dfa9497c9abe1e3
Prince9510 <satasiyaprince68@gmail.com>
2025-05-01 18:24:39 +0530

    operators

--- operators & expressions/operators/comparison_operators.js
+++ operators & expressions/operators/comparison_operators.js
@@ -28,8 +28,8 @@ console.log(5 < 3);     // false
 // Greater than or equal to (>=)
 
 
-console.log(5 >= 5);    // true
+console.log(5 >= 5);// true
 
 
 // Less than or equal to (<=)
-console.log(5 <= 3);    // false
+console.log(5 <= 3);// false
